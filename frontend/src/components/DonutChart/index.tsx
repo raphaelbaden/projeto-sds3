5dd61411dc332e0b2072eabbb0a491531658cec0
“raphaelbaden” <raphaelbaden@gmail.com>
2021-05-08 08:25:24 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,25 +11,19 @@ type ChartData = {
 
 const DonutChart = () => {
     
-    // FORMA ERRADA
-    let chartData : ChartData = { labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    //Existe essas duas formas de concatenar
-    //axios.get(BASE_URL + '/sales/amount-by-seller');
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then((response) => { //vamos definir uma função dentro do THEN para ser executada quando a requisição GET chegar com SUCESSO
-                              //response -> é a resposta (response.data -> corpo da resposta)
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then((response) => { //vamos definir uma função dentro do THEN para ser executada quando a requisição GET chegar com SUCESSO
+                                //response -> é a resposta (response.data -> corpo da resposta)
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            chartData = { labels: myLabels, series: mySeries };
-        }) 
-    
-    /*const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }*/
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
     
     const options = {
         legend: {
--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'html://localhost:8080';
+export const BASE_URL = 'http://localhost:8080';
